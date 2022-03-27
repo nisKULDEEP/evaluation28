7b3fb201e01abc3ca326924cf8fa5927533a4b71
niskuldeep <kuldeepnishad.singh@gmail.com>
2022-03-27 09:52:13 +0530

    all is done

---
+++ src/Components/Button.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+let Button = styled.button`
+  padding: 5px 10px;
+  color: #ffffff;
+  background-color: black;
+  font-weight: 400;
+  border-radius: 5px;
+  margin-left: 10px;
+
+  &:hover {
+    color: black;
+    background-color: #ffffff;
+    border: 1px solid black;
+    cursor: pointer;
+  }
+`;
+
+export default Button;
